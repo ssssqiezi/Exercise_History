53a3af3093d508b42d78591198c06c3a9738df46
renwj <ssssqiezi@163.com>
2015-11-14 18:32:56 +0800

    add the aomticboolean type to keep the aotomic operation

--- Exercise_AutomaticBoolean.java
+++ Exercise_AutomaticBoolean.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Exercise_AutomaticBoolean {
 
@@ -49,14 +50,83 @@ public class Exercise_AutomaticBoolean {
     }
 
 
+    private static class BarWorkAutomatic implements Runnable{
+
+        private String  name ;
+
+        public BarWorkAutomatic(String name) {
+            this.name = name;
+        }
+
+        private static AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+        @Override
+        public void run() {
+            try {
+                if (atomicBoolean.compareAndSet(false, true)) {
+                    System.out.println(name + "Enter");
+                    try {
+                        System.out.println(name+"working");
+                        TimeUnit.SECONDS.sleep(2);
+                    } catch (InterruptedException e1) {
+                        // do nothing
+                    }
+                    System.out.println(name+"leave");
+                    atomicBoolean.set(false);
+                }
+            } catch (Exception e) {
+                System.out.println(name+"give up ");
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+
+
+    private static class BarWorkerTest implements Runnable {
+
+        private static AtomicBoolean exists = new AtomicBoolean(false);
+
+        private String name;
+
+        public BarWorkerTest(String name) {
+            this.name = name;
+        }
+
+        public void run() {
+            if (exists.compareAndSet(false, true)) {
+                System.out.println(name + " enter");
+                try {
+                    System.out.println(name + " working");
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException e) {
+                    // do nothing
+                }
+                System.out.println(name + " leave");
+                exists.set(false);
+            }else{
+                System.out.println(name + " give up");
+            }
+        }
+
+    }
+
+
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         System.out.println("Hello World!");
         System.out.println("----程序开始运行----");
 
-        new  Thread(new BarWorker("Test1")).start();
-        new Thread(new BarWorker("Test2")).start();
+        //1: to test the muti-thread, I use the run method
+/*        new  Thread(new BarWorker("Test1")).start();
+        new Thread(new BarWorker("Test2")).start();*/
 
+        //my code ,no exception so can not follow the other thread
+ /*       new Thread(new BarWorkAutomatic("AutoTest1")).start();
+        new Thread(new BarWorkAutomatic("AutoTest2")).start();*/
 
+        // the other code, we can see the give up operation
+        new Thread(new BarWorkerTest("AutoTest1")).start();
+        new Thread(new BarWorkerTest("AutoTest2")).start();
     }
 
     }
